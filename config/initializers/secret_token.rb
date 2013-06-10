64d3735ef0c3335771beadf7ec3a17c726aded5a
Jaap Groeneveld <jgroeneveld@me.com>
2013-06-10 12:13:18 +0200

    init with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sonitus::Application.config.secret_key_base = 'a27e639f548331d93ef47031e75adc9761930635276a15b77ab0708ba258fa96f605885e779ed9c11d0743526f7e6b4ea9917e4e04afe36d7150e9e6856aa03d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sonitus::Application.config.session_store :cookie_store, key: '_sonitus_session'
