37bf4d18446c3b03c7866f77cae4c145e2f5315d
Jaap Groeneveld <jgroeneveld@me.com>
2013-06-18 13:43:41 +0200

    moved default_url definition to AlbumImageUploader

--- app/uploaders/album_image_uploader.rb
+++ app/uploaders/album_image_uploader.rb
@@ -9,6 +9,14 @@ class AlbumImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url
+    if version_name == 'for_collection'
+      "http://placehold.it/220x220"
+    else
+      "http://placehold.it/580x580"
+    end
+  end
+
   process resize_and_pad: [580, 580]
 
   version :for_collection do
